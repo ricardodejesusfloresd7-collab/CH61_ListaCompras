4eceeaccc116286eefb017f395517c20043d45bc
Ricardo Flores <ricardodejesusfloresd7@gmail.com>
2025-11-21 11:25:29 -0600

    Subo actividad de listacompras

---
+++ js/main.js
@@ -0,0 +1,81 @@
+const txtName = document.getElementById("Name");
+const txtNumber=document.getElementById("Number");
+const btnAgregar=document.getElementById("btnAgregar");
+const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const alertValidaciones=document.getElementById("alertValidaciones");
+const contadorProductos=document.getElementById("contadorProductos");
+const productosTotal=document.getElementById("productosTotal");
+const precioTotal=document.getElementById("precioTotal");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+
+let cont = 0;
+let totalEnProductos = 0;
+let costoTotal = 0;
+
+function validarCantidad(cantidad){
+    if(cantidad.length==0){
+        return false;
+    }
+    if(isNaN(cantidad)){
+        return false;
+    }
+    if(Number(cantidad) <=0){
+        return false;
+    }//<=0
+    return true;
+} //validadCantidad
+
+function getPrecio(){
+    return Math.round(Math.random()*100000)/100;
+}//obtenerPrecio
+
+btnAgregar.addEventListener("click", function(event){
+    event.preventDefault();
+    let isValid = true;
+    txtName.style.border= "";
+    txtNumber.style.border="";
+    alertValidacionesTexto.innerHTML= "";
+    alertValidaciones.style.display = "none";
+
+    
+    if(txtName.value.length <3){
+        txtName.style.border = "solid medium red";
+        alertValidacionesTexto.innerHTML= "<strong>El nombre del producto no es correcto</strong><br/>";
+        alertValidaciones.style.display = "block";
+        isValid = false;
+
+    }
+    if(! validarCantidad(txtNumber.value)){
+        txtNumber.style.border = "solid medium red";
+        alertValidacionesTexto.innerHTML+= "<strong>La cantidad no es correcta</strong>";
+        alertValidaciones.style.display = "block";
+        isValid = false;
+
+    }// 
+
+    if(isValid){
+        let precio = getPrecio();
+        console.log(precio);
+        cont++;
+        let row = `<tr>
+                    <td>${cont}</td>
+                    <td>${txtName.value}</td>
+                    <td>${txtNumber.value}</td>
+                    <td>${precio}</td>
+                </tr>`;
+
+        totalEnProductos+= Number(txtNumber.value);
+        costoTotal += precio * Number (txtNumber.value);
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        contadorProductos.innertext = cont;
+        productosTotal.innerText = totalEnProductos;
+        precioTotal.innerText=new Intl.NumberFormat("es-MX", 
+            { style: "currency", currency: "MXN" }).format(costoTotal);
+        
+        txtName.value = "";
+        txtNumber.value = "";
+        txtName.focus();
+    }
+    
+});
